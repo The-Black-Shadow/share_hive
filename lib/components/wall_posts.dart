45335db01b50683e2956c907b4d3a2628b420021
The-Black-Shadow <mehedihasan.cse18@gmail.com>
2024-04-17 11:18:04 +0600

    finished basic firebase logics

---
+++ lib/components/wall_posts.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class WallPost extends StatelessWidget {
+  final String message;
+  final String user;
+
+  const WallPost({
+    super.key,
+    required this.message,
+    required this.user,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Card(
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        color: const Color.fromARGB(
+            255, 68, 255, 233), // You can change the color as needed
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                user,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                message,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:share_hive/components/textfield.dart';
+import 'package:share_hive/components/wall_posts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,32 +13,112 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final User? user = FirebaseAuth.instance.currentUser;
+  TextEditingController textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: const Text('Share Hive'),
+        backgroundColor: Colors.grey[900],
+        title: Text(
+          'Share Hive',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24.0,
+            fontFamily: GoogleFonts.pacifico().fontFamily,
+          ),
+        ),
+        elevation: 0,
         actions: [
           IconButton(
-            icon: const Icon(Icons.logout),
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
             onPressed: () {
               _logout(context);
             },
           ),
         ],
       ),
-      body: const Placeholder(),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('User Posts')
+                    .orderBy(
+                      'timestamp',
+                      descending: true,
+                    )
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children:
+                          List.generate(snapshot.data!.docs.length, (index) {
+                        final post = snapshot.data!.docs[index];
+                        return WallPost(
+                          message: post['Message'],
+                          user: post['UserEmail'],
+                        );
+                      }),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('Error: ${snapshot.error}'),
+                    );
+                  }
+                  return const Center(child: CircularProgressIndicator());
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: MyTextfield(
+                    controller: textController,
+                    hint: 'Write something on the wall...',
+                    obscureText: false,
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () {
+                    PostMessage();
+                  },
+                ),
+              ],
+            ),
+          ),
+          Text('Logged In as ${user?.email}'),
+        ],
+      ),
     );
   }
-}
 
-Future<void> _logout(BuildContext context) async {
-  try {
-    await FirebaseAuth.instance.signOut();
-    // Navigate to the login screen or any other screen after logout.
-    Navigator.of(context).pushReplacementNamed('/login');
-  } catch (e) {
-    print('Error logging out: $e');
-    // Handle error, if any.
+  Future<void> _logout(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print('Error logging out: $e');
+    }
+  }
+
+  void PostMessage() {
+    if (textController.text.isNotEmpty) {
+      FirebaseFirestore.instance.collection("User Posts").add({
+        'UserEmail': user?.email,
+        'Message': textController.text,
+        'timestamp': Timestamp.now(),
+      });
+      textController.clear();
+    }
   }
 }
